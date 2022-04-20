b2554e4bb7dabc271886c68476a2df23715fb7d4
Jonathan Weinreich <jonathan@fourcube.net>
2022-04-20 11:21:49 +0100

    Release 5445: JSON.parse 2

--- utils/fetchGraphQL.ts
+++ utils/fetchGraphQL.ts
@@ -89,10 +89,10 @@ options: {
 
   return fetchWithTimeoutIO(5000)(`http://${keystoneHost}:3000/api/graphql`, {
     method: 'POST',
-    body: JSON.stringify({
+    body: JSON.parse(JSON.stringify({
       query: print(getDocumentNode(options.operation)),
       variables: options.variables,
-    }),
+    })),
     headers: {
       'Content-Type': 'application/json',
       'x-api-key': keystoneNextjsBuildApiKey,
